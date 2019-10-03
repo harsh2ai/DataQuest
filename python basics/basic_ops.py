4ce795ba5f9d90983c4cb2bdcfc9e74258e8924e
Komatipolu Mokshagna Sai Teja <mokshagnasaiteja517@gmail.com>
2019-10-03 21:14:59 +0530

    Added basic operation in python

---
+++ python basics/basic_ops.py
@@ -0,0 +1,37 @@
+#Arithmetic Operations
+a=2
+b=3
+
+#Additon of two numbers
+add=a+b
+
+#Subtraction of two numbers
+sub=a-b
+
+#Multiplication of two numbers
+mul=a*b
+
+#Division(Float) of two numbers
+div=a/b
+
+#Division(Floor) of two numbers
+div_fl=a//b
+
+#modular_divison of two numbers
+rem=a%b
+
+#exponent of a number
+exp=a**b
+
+#Printing the results
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Float division:", div)
+print("Integer division or Floor division:", div_fl)
+print("Remainder:", rem)
+print("exponent operation:", exp)
+
+
+
+
