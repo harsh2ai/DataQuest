722276a25f9d7ae9d3a74b8b065c18dde42c2b8c
Víctor Salazar <victor.salazar@trescloud.com>
2019-10-03 12:00:01 -0500

    [add] manejo strings

---
+++ python basics/strings.py
@@ -0,0 +1,12 @@
+
+myStr = "Hola mundo"
+#Convierte todo el string a mayusculas
+print(myStr.upper())
+#Convierte todo el string a minuscula
+print(myStr.lower())
+#Convierte la primera letra del string a mayusculas
+print(myStr.capitalize())
+
+#Reemplaza carater o caracteres por uno nuevo
+print(myStr.replace('a', 'A'))
+print(myStr.replace('Hola', 'hello'))
