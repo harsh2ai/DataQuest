b96581c6e0726c8b94077efc6a4b61d307470962
ghm12 <oghm12@hotmail.com>
2019-10-05 13:00:36 -0300

    Added a string manipulation guide.

--- python basics/string_manipulation.py
+++ python basics/string_manipulation.py
@@ -28,6 +28,8 @@ string = ' hello, world! '
 #Removes the extra spaces from left and right.
 print('Strip:', string.strip())
 
+string = ' hello, world! '
+
 #Removes the extra spaces from the right.
 print('Right strip:', string.rstrip())
 
@@ -36,6 +38,12 @@ print('Left strip:', string.lstrip())
 
 #Splits the current string into a list of strings.
 #In this case, every list element is separated from another when there's a comma in the original string.
+#Removes the extra spaces both from left and right.
+print('Strip:', string.strip())
+
+#Splits the current string into a list of strings.
+#In this case, every list element is separated from another
+#when there's a comma in the original string.
 #If no argument is given, it splits where it finds a blank space.
 string_split = string.split(',')
 print(string_split)
@@ -63,22 +71,26 @@ print('Position 0 to 4:', string[:5])
 #Returns from character in position 7 until the end.
 print('Position 7 to end:', string[7:])
 
-#Returns from character in position 3 until character in position 5.
+#Returns from character in position 3 until 
+#character in position 5.
 print('Position 3 to 5:', string[3:6])
-#Just like the first example, it uses :6 but doesn't include position 6 in the output.
+#Just like the first example, it uses :6 but doesn't
+#include position 6 in the output
 
-#Returns from character in position 3 until character in position 9, picking a character every 2 characters.
+#Returns from character in position 3 until character 
+#in position 9, picking a character every 2 characters.
 print('Position 3 to 8, every 2 characters:', string[3:9:2])
-#As explained above, even though we use :9, the character in position 9 is not included in the output.
+#As explained above, even though we use :9, the character
+#in position 9 is not included in the output.
 
 #The same way, we can pick a string for every character, but backwards.
 print('Reversed:', string[::-1])
-#Note we needed to put :: before -1 because the first two arguments are used for position, just like demonstrated above.
+#Note we needed to put :: before -1 because the first two arguments
+#are used for position, just like demonstrated above.
 
 #Returns the position of a set of characters in a string
 print('Position of "world!":', string.find('world!'))
-#Note it returns only the position of the first character of what you're searching for.
-#If there is more than one ocurrance of what you're searching for, it returns the first.
+#Note it returns only the position of the first letter of what you're searching for.
 
-#If such set of characters is not found, it returns -1.
+#If such set of characters is not found, it'll return -1
 print('Position of "Hola!":', string.find('Hola!'))
