214b80151329db96b7280a2d617a6c648746866a
ghm12 <oghm12@hotmail.com>
2019-10-05 14:27:35 -0300

    Fixed things for easier understanding.

--- python basics/string_manipulation.py
+++ python basics/string_manipulation.py
@@ -1,4 +1,6 @@
 #String manipulation
+#Strings are "a list of characeters", and here you can learn a bit of how to manipulate them.
+
 string = "hello, world!"
 
 #Capitalizes the first letter of the string.
@@ -14,12 +16,17 @@ print('Title:', string.title())
 #Turns every letter into upper case.
 print('Uppercase:', string.upper())
 
+#Now with a full-uppercase string.
 string = 'HELLO, WORLD!'
 
 #Turns every letter into lower case.
 print('Lowercase:', string.lower())
 
+#Now with extra spaces in the string.
 string = ' hello, world! '
+
+#Removes the extra spaces from left and right.
+print('Strip:', string.strip())
 
 #Removes the extra spaces from the right.
 print('Right strip:', string.rstrip())
@@ -27,12 +34,8 @@ print('Right strip:', string.rstrip())
 #Removes the extra spaces from the left.
 print('Left strip:', string.lstrip())
 
-#Removes the extra spaces both from left and right.
-print('Strip:', string.strip())
-
 #Splits the current string into a list of strings.
-#In this case, every list element is separated from another
-#when there's a comma in the original string.
+#In this case, every list element is separated from another when there's a comma in the original string.
 #If no argument is given, it splits where it finds a blank space.
 string_split = string.split(',')
 print(string_split)
@@ -47,41 +50,35 @@ print('Separate characters:', '-'.join(string))
 #Or to separate strings from a list of strings when joining them.
 print('Separate strings:', '-'.join(string_split))
 
-#Strings are "a list of characters", and you can access
-#specific parts of a string just like a list.
+#Strings are "a list of characters", and you can access specific parts of a string just like a list.
 string = 'Hello, world!'
 
 #Returns the very first character of a string.
 print('Position 0:', string[0])
 
-#Returns from character in position 0 until 
-#character in position 4.
+#Returns from character in position 0 until character in position 4.
 print('Position 0 to 4:', string[:5])
 #Note it uses [:5], but it doesn't include position 5 in the output.
 
 #Returns from character in position 7 until the end.
 print('Position 7 to end:', string[7:])
 
-#Returns from character in position 3 until 
-#character in position 5.
+#Returns from character in position 3 until character in position 5.
 print('Position 3 to 5:', string[3:6])
 #Just like the first example, it uses :6 but doesn't
 #include position 6 in the output
 
-#Returns from character in position 3 until character 
-#in position 9, picking a character every 2 characters.
+#Returns from character in position 3 until character in position 9, picking a character every 2 characters.
 print('Position 3 to 8, every 2 characters:', string[3:9:2])
-#As explained above, even though we use :9, the character
-#in position 9 is not included in the output.
+#As explained above, even though we use :9, the character in position 9 is not included in the output.
 
 #The same way, we can pick a string for every character, but backwards.
 print('Reversed:', string[::-1])
-#Note we needed to put :: before -1 because the first two arguments
-#are used for position, just like demonstrated above.
+#Note we needed to put :: before -1 because the first two arguments are used for position, just like demonstrated above.
 
 #Returns the position of a set of characters in a string
 print('Position of "world!":', string.find('world!'))
 #Note it returns only the position of the first letter of what you're searching for.
 
-#If such set of characters is not found, it'll return -1
+#If such set of characters is not found, it returns -1
 print('Position of "Hola!":', string.find('Hola!'))
