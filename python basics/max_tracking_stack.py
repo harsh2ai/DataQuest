d94b2709d2475530758162196977f2058f2fd218
N Abhishek <13452346+Kodatos@users.noreply.github.com>
2019-10-14 17:28:44 +0530

    Create max_tracking_stack.py

---
+++ python basics/max_tracking_stack.py
@@ -0,0 +1,40 @@
+# Kodatos 2019
+
+class StackWithMax: 
+	def __init__(self): 
+		
+		# main stack 
+		self.mainStack = [] 
+	
+		# tack to keep track of 
+		# max element 
+		self.trackStack = [] 
+
+	def push(self, x): 
+		self.mainStack.append(x) 
+		if (len(self.mainStack) == 1): 
+			self.trackStack.append(x) 
+			return
+ 
+		if (x > self.trackStack[-1]): 
+			self.trackStack.append(x) 
+		else: 
+			self.trackStack.append(self.trackStack[-1]) 
+
+	def getMax(self): 
+		return self.trackStack[-1] 
+
+	def pop(self): 
+		self.mainStack.pop() 
+		self.trackStack.pop() 
+
+# Driver Code 
+if __name__ == '__main__': 
+
+	s = StackWithMax() 
+	s.push(20) 
+	print(s.getMax()) 
+	s.push(10) 
+	print(s.getMax()) 
+	s.push(50) 
+	print(s.getMax()) 
